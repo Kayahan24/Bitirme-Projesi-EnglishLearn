71a1ec770bc8d3d93213d5714e37748011073bdd
Mervan-Torlak <mervan.torlak@ogr.sakarya.edu.tr>
2023-06-01 19:57:39 +0300

    Finish Project

---
+++ lib/widgets/content_area.dart
@@ -0,0 +1,35 @@
+import 'package:bitirme_english/configs/themes/app_colors.dart';
+import 'package:bitirme_english/configs/themes/ui_parameters.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/cupertino.dart';
+
+class ContentArea extends StatelessWidget {
+  final bool addPadding;
+  final Widget child;
+  const ContentArea({Key? key, this.addPadding = true, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: const BorderRadius.vertical(
+        top: Radius.circular(20),
+      ),
+      clipBehavior: Clip.hardEdge,
+      type: MaterialType.transparency,
+      child: Ink(
+        decoration: BoxDecoration(color: customScaffoldColor(context)),
+        padding: addPadding
+            ? EdgeInsets.only(
+                top: mobileScreenPadding,
+                left: mobileScreenPadding,
+                right: mobileScreenPadding,
+              )
+            : EdgeInsets.zero,
+        child: child,
+      ),
+    );
+  }
+}
