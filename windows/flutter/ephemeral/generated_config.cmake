82b498285c20b5ad20cea3ff2b5dc5a8436e425d
Mervan-Torlak <mervan.torlak@ogr.sakarya.edu.tr>
2023-06-06 19:34:14 +0300

    a

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Mervan\\Gelistirme\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Mervan\\Gelistirme\\projects\\bitirme_english" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Mervan\\Gelistirme\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Users\\Mervan\\Gelistirme\\projects\\bitirme_english"
+  "FLUTTER_ROOT=C:\\Users\\Mervan\\Gelistirme\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Mervan\\Gelistirme\\projects\\bitirme_english\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Mervan\\Gelistirme\\projects\\bitirme_english"
+  "FLUTTER_TARGET=C:\\Users\\Mervan\\Gelistirme\\projects\\bitirme_english\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Mervan\\Gelistirme\\projects\\bitirme_english\\.dart_tool\\package_config.json"
+)
